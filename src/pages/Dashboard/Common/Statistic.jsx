a2e28cffcd251cedef62f73960f1a2a557e3a3f8
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-31 00:12:25 +0600

    host statistic added

--- src/pages/Dashboard/Common/Statistic.jsx
+++ src/pages/Dashboard/Common/Statistic.jsx
@@ -1,5 +1,6 @@
 import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
 import useRole from "../../../hooks/useRole";
+import HostStatistics from "../Host/HostStatistic";
 import AdminStatistics from "../admin/AdminStatistics";
 
 const Statistic = () => {
@@ -12,6 +13,9 @@ const Statistic = () => {
             {/* <h1 className="text-5xl text-black">welcome to statistics page</h1> */}
             {
                 role?.role === 'admin' && <AdminStatistics />
+            }
+            {
+                role?.role === 'host' && <HostStatistics />
             }
         </>
     );
